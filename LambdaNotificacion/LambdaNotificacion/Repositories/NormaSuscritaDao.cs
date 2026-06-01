b41d2c3cbfe02d90ddf048306a6cbf042276a72f
Benjamín <benjamin.diaze@outlook.com>
2026-06-01 02:28:45 -0400

    Se reemplaza inicialización de parámetros NormaSuscritaDao por Lazy.

--- LambdaNotificacion/LambdaNotificacion/Repositories/NormaSuscritaDao.cs
+++ LambdaNotificacion/LambdaNotificacion/Repositories/NormaSuscritaDao.cs
@@ -10,11 +10,6 @@ using System.Threading.Tasks;
 
 namespace LambdaNotificacion.Repositories {
     internal class NormaSuscritaDao(VariableEntornoHelper variableEntorno, ParameterStoreHelper parameterStore, SecretManagerHelper secretManagerHelper, ClientCredentialsHelper clientCredentialsHelper) {
-        private readonly string _baseUrl = parameterStore.ObtenerParametro(variableEntorno.Obtener("ARN_PARAMETER_TANATOS_API_URL")).Result;
-        private readonly string _cognitoBaseUrl = JsonSerializer.Deserialize<Dictionary<string, string>>(secretManagerHelper.ObtenerSecreto(variableEntorno.Obtener("ARN_SECRET_TANATOS_API")).Result)!["CognitoBaseUrl"];
-        private readonly string _notificacionesClientId = JsonSerializer.Deserialize<Dictionary<string, string>>(secretManagerHelper.ObtenerSecreto(variableEntorno.Obtener("ARN_SECRET_TANATOS_API")).Result)!["NotificacionesUserPoolClientId"];
-        private readonly string _notificacionesClientSecret = JsonSerializer.Deserialize<Dictionary<string, string>>(secretManagerHelper.ObtenerSecreto(variableEntorno.Obtener("ARN_SECRET_TANATOS_API")).Result)!["NotificacionesUserPoolClientSecret"];
-
         private readonly string[] _notificacionesScopes = [
 			"api/negocios.read.all",
 			"api/obligaciones.read.all",
@@ -24,11 +19,36 @@ namespace LambdaNotificacion.Repositories {
 			"api/templates.read.public"
 		];
 
-		public async Task ProcesarNotificacion(string rawJson) {
-            using HttpClient client = new(new RetryHandler(new HttpClientHandler()));
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", await clientCredentialsHelper.ObtenerAccessToken(_cognitoBaseUrl, _notificacionesClientId, _notificacionesClientSecret, _notificacionesScopes));
+		private readonly Lazy<Task<ApiConfig>> _config = new(() => InicializarApiConfig(variableEntorno, parameterStore, secretManagerHelper));
 
-            HttpResponseMessage response = await client.PostAsync(_baseUrl + "/NormaSuscrita/ProcesarNotificacion", new StringContent(rawJson, Encoding.UTF8, "application/json"));
+		private static async Task<ApiConfig> InicializarApiConfig(VariableEntornoHelper variableEntorno, ParameterStoreHelper parameterStore, SecretManagerHelper secretManagerHelper) {
+			Task<string> taskParametro = parameterStore.ObtenerParametro(variableEntorno.Obtener("ARN_PARAMETER_TANATOS_API_URL"));
+			Task<string> taskSecreto = secretManagerHelper.ObtenerSecreto(variableEntorno.Obtener("ARN_SECRET_TANATOS_API"));
+
+			await Task.WhenAll(taskParametro, taskSecreto);
+			string baseUrl = taskParametro.Result;
+			Dictionary<string, string> secretos = JsonSerializer.Deserialize<Dictionary<string, string>>(taskSecreto.Result)!;
+
+			return new ApiConfig {
+				BaseUrl = baseUrl,
+				CognitoBaseUrl = secretos["CognitoBaseUrl"],
+				NotificacionesClientId = secretos["NotificacionesUserPoolClientId"],
+				NotificacionesClientSecret = secretos["NotificacionesUserPoolClientSecret"],
+			};
+		}
+
+		public async Task ProcesarNotificacion(string rawJson) {
+			ApiConfig config = await _config.Value;
+
+			using HttpClient client = new(new RetryHandler(new HttpClientHandler()));
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", await clientCredentialsHelper.ObtenerAccessToken(
+				config.CognitoBaseUrl,
+				config.NotificacionesClientId,
+				config.NotificacionesClientSecret, 
+				_notificacionesScopes
+			));
+
+            HttpResponseMessage response = await client.PostAsync(config.BaseUrl + "/NormaSuscrita/ProcesarNotificacion", new StringContent(rawJson, Encoding.UTF8, "application/json"));
 			if (!response.IsSuccessStatusCode) {
 				throw new HttpRequestException(
 					$"Ocurrió un error al procesar la notificación. StatusCode: {response.StatusCode} - Content: {await response.Content.ReadAsStringAsync()}",
@@ -37,5 +57,12 @@ namespace LambdaNotificacion.Repositories {
 				);
 			}
 		}
-    }
+
+		internal class ApiConfig {
+			public required string BaseUrl { get; init; }
+			public required string CognitoBaseUrl { get; init; }
+			public required string NotificacionesClientId { get; init; }
+			public required string NotificacionesClientSecret { get; init; }
+		}
+	}
 }
